6c33da96b3115723deb44dd6b1e5b91b9ebf8426
Corentin BEAL <coco.bea@hotmail.fr>
2017-03-29 11:55:27 +0200

    Ajout de commentaires

--- js/navigation.js
+++ js/navigation.js
@@ -1,34 +1,38 @@
+//La fonction permet d' afficher une page voulue en cachant les autres
+//param page_dest Une String qui contient l'élément à afficher
 function navigateTo(page_dest){
-  var pages = document.querySelectorAll('[data-page]');
+  var pages = document.querySelectorAll('[data-page]');   //Récupération de tous les événements
   for(var i=0; i<pages.length; i++){
     var page = pages[i];
-    if(page.getAttribute('data-page') == page_dest){
-      page.classList.remove('hidden');
+    if(page.getAttribute('data-page') == page_dest){    //Si l'élément est celui que l'on veut acheter
+      page.classList.remove('hidden');    //On affiche l'élément
     }else{
-      page.classList.add('hidden');
+      page.classList.add('hidden');       //Sinon on cache l'éléemnt
     }
   }
 }
 
+//La fonction permet de mettre à jour la liste des conversations
+//param conversations La liste de toutes les conversations
 function updateConversations(conversations){
   var main_div = document.getElementById('conversations');
 
   for(var i=0; i<conversations.length; i++){
     var conversation = conversations[i];
-    var conv_exist = document.querySelectorAll('.conversation[data-id="' + conversation.id + '"]');
+    var conv_exist = document.querySelectorAll('.conversation[data-id="' + conversation.id + '"]'); //On vérifie si la conversation est déjà présente
 
     if(conv_exist.length == 0){  //Si la conversation n'existe pas encore alors on la rajoute
       var conv_div = document.createElement('div');
       conv_div.classList.add('conversation');
-      conv_div.setAttribute('data-id', conversation.id);
-      conv_div.addEventListener("click", switchConversation);
+      conv_div.setAttribute('data-id', conversation.id);    //On stocke l'id de la conversation
+      conv_div.addEventListener("click", switchConversation); //On défini le onClick
 
       var conv_status = document.createElement('div');
       conv_status.classList.add('status');
 
       var conv_name = document.createElement('p');
       conv_name.classList.add('name');
-      conv_name.innerHTML = conversation.name;
+      conv_name.innerHTML = conversation.name;    //Affichage du nom de la conversation
 
       conv_div.appendChild(conv_status);
       conv_div.appendChild(conv_name);
@@ -36,35 +40,37 @@ function updateConversations(conversations){
     }
   }
 
-  scrollMessages();
+  scrollMessages();   //On scroll les messages
 }
 
+//La fonction permet de changer les messages
+//param event L' événement du bouton
 function switchConversation(event){
   var target = event.target;
 
   var conversation_div = target;
-  if(!conversation_div.getAttribute('data-id')){
+  if(!conversation_div.getAttribute('data-id')){    //On récupére l'id la conversation sélectionnée
       conversation_div = target.parentNode;
   }
 
   var conversation_id = conversation_div.getAttribute('data-id');
   var status = conversation_div.querySelector('.status');
-  status.classList.remove('new');
+  status.classList.remove('new');   //On reset le status
 
   var conversation;
-  for(var i=0; i<conversations.length; i++){
+  for(var i=0; i<conversations.length; i++){    //On cherche la conversation dans la liste en JS pour définir la conversation actuelle
     if(conversations[i].id == conversation_id){
       conversation = conversations[i];
       break;
     }
   }
 
-  clearMessages();
+  clearMessages();    //On vide les messages
 
-  document.getElementById('conversation_title').innerHTML = conversation.name;
+  document.getElementById('conversation_title').innerHTML = conversation.name;    //On rempli le nom de la conversation
 
   var current_messages = conversation.messages;
-  if(current_messages){
+  if(current_messages){     //Si on a déjà chargé des messages alors on les ajoutent
     for(var j=0; j<current_messages.length; j++){
       addNewMessage(current_messages[j]);
     }
@@ -73,6 +79,8 @@ function switchConversation(event){
   current_conversation = conversation;
 }
 
+//La fonction permet d'afficher un nouveau message dans l'affichage principal
+//param message Un objet message
 function addNewMessage(message){
   var message_div = document.getElementById('messages');
   message_div.appendChild(getMessageDiv(message));
@@ -80,17 +88,22 @@ function addNewMessage(message){
   scrollMessages();
 }
 
+//La fonction va vider les messages dans l'affichage principal
 function clearMessages(){
   var message_div = document.getElementById('messages');
   message_div.innerHTML = '';
 }
 
+//La fonction permet de récupérer des infos un utilisateur déjà chargé selon son id
+//param user_id L'id de l'utilisateur à récupérer
 function getUser(user_id){
   if(user_map[user_id]){
     return user_map[user_id];
   }
 }
 
+//La fonction récupére le nom de l'utilisateur si il est déjà chargé
+//param user_id L'id de l'utilisateur à récupérer
 function getUserDiv(user_id){
   var user = getUser(user_id);
 
@@ -99,14 +112,16 @@ function getUserDiv(user_id){
   }
 }
 
+//La fonction permet de créer une div de message compléte
+//param message Un objet message avec le contenu du message et l'id de l'utilisateur
 function getMessageDiv(message){
   var message_div = document.createElement('div');
   message_div.classList.add('message');
-  message_div.setAttribute('data-id', message.id);
+  message_div.setAttribute('data-id', message.id);    //On stocke l'id du message
 
   var profil_img = document.createElement('img');
   profil_img.classList.add('profile');
-  profil_img.src = SERVER_URL + '/user/profile/' + message.user_id;
+  profil_img.src = SERVER_URL + '/user/profile/' + message.user_id;   //On initialise l'url de la photo de profil
 
   var message_elem = document.createElement('div');
   message_elem.classList.add('chat');
@@ -117,16 +132,16 @@ function getMessageDiv(message){
 
   var username = getUserDiv(message.user_id);
 
-  if(username){
-    username_elem.innerHTML = username;
+  if(username){     //On vérifie si le username est connu
+    username_elem.innerHTML = username;   //On l'affiche
   }else{
-    username_elem.classList.add('lazy-load');
-    if(!user_to_load.includes(message.user_id)){
+    username_elem.classList.add('lazy-load');   //On ajoute la class lazy-load + un attribut pour le charger plus tard
+    if(!user_to_load.includes(message.user_id)){    //Si l'id de l'user n'est pas déjà recherché alors on l'ajoute à la liste de recherche
       user_to_load.push(message.user_id);
     }
   }
 
-  var content_elem = document.createElement('p');
+  var content_elem = document.createElement('p');     //Remplissage du contenu du message
   content_elem.setAttribute('data-id', message.id);
   content_elem.classList.add('content');
   content_elem.innerHTML = message.content;
@@ -136,35 +151,35 @@ function getMessageDiv(message){
 
   message_div.appendChild(profil_img);
   message_div.appendChild(message_elem);
-  /*message_div.appendChild(username_elem);
-  message_div.appendChild(message_elem);*/
 
   return message_div;
 }
 
+//La fonction qui est appelée quand on appuye sur envoyer
 function sendNewMessage(){
-  var message = document.getElementById('chat_box').value;
+  var message = document.getElementById('chat_box').value;    //On récupére le contenu du message
 
-  createMessage(message, current_conversation.id).then(function (data) {
+  createMessage(message, current_conversation.id).then(function (data) {    //Envoie du message à l'API
       var message_obj = JSON.parse(data)    //On récupére une liste d'objet Message JSON
 
-      if(!current_conversation.messages){
+      if(!current_conversation.messages){   //Si la conversation n'a pas de message alors on l'initialise
           current_conversation.messages = [];
       }
 
-      current_conversation.messages.push(message_obj);
-      addNewMessage(message_obj);
+      current_conversation.messages.push(message_obj);    //On ajoute le message
+      addNewMessage(message_obj);     //On affiche le message
   }, function (err) {
       console.log(err);
   });
 
 }
 
+//La fonction est appelé quand on cherche une nouvelle conversation
 function searchConversation(){
   searchPublicConversation().then(function (data) {
-      var debug = JSON.parse(data)    //On récupére une liste d'objet Message JSON
+      var debug = JSON.parse(data)    //On récupére une liste d'objet Conversation JSON
 
-      var conv_div = document.getElementById('conversations_join');
+      var conv_div = document.getElementById('conversations_join');   //On crée les éléments qui seront affichés dans la liste
       conv_div.innerHTML = '';
       for(var i =0; i<debug.length; i++){
         var new_conv = document.createElement('div');
@@ -173,11 +188,11 @@ function searchConversation(){
         conv_text.innerHTML = debug[i].name;
 
         var conv_button = document.createElement('button');
-        conv_button.setAttribute('data-id', debug[i].id);
-        conv_button.addEventListener("click", function(elem){
+        conv_button.setAttribute('data-id', debug[i].id);   //On donne l'id de la conversation
+        conv_button.addEventListener("click", function(elem){   //On ajoute la possiblité de rejoindre en cliquant sur un bouton
             var button = elem.target;
             var conversation_id = button.getAttribute('data-id');
-            joinConversation(conversation_id);
+            joinConversation(conversation_id);    //On appelle la fonction pour rejoindre
         });
         conv_button.innerHTML = "Join";
 
@@ -191,6 +206,7 @@ function searchConversation(){
   });
 }
 
+//La fonction permet de faire scroller vers les messages tout en bas
 function scrollMessages(){
   var messages = document.getElementById('messages');
   messages.scrollTop = messages.scrollHeight
