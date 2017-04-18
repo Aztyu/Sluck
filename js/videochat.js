7607615356033903b255e3a27d8c45d62fe6b046
Corentin BEAL <corentin.beal@winciel.fr>
2017-04-18 16:07:50 +0200

    Essai de partage video

--- js/videochat.js
+++ js/videochat.js
@@ -5,7 +5,11 @@ function startPeerConnection(){
   peer = new Peer({
     host: '193.70.38.229',
     port: 3389,
-    path: '/'
+    path: '/',
+    config: {'iceServers': [
+      { url: 'stun:stun1.l.google.com:19302' },
+      { url: 'turn:193.70.38.229', credential: 'sluck_kiki', username: 'sluck_api' }
+    ]}
   });
 
   console.log(peer);
@@ -27,15 +31,28 @@ function startPeerConnection(){
       // Send messages
       connection.send('Hello!');
     });
-    /*connection.on('open', function(data){
-      console.log(data);
-    });
+  });
 
-    connection.on('data', function(data) {
-      console.log('Received', data);
-    });*/
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+  peer.on('call', function(call) {
+    navigator.getUserMedia({video: true, audio: true}, function(stream) {
+      call.answer(stream); // Answer the call with an A/V stream.
+
+      call.on('stream', function(remoteStream) {
+        console.log('Appel entrant' + remoteStream);
+
+        var video = document.getElementById('webrtcvideo');
+        video.src = (URL || webkitURL || mozURL).createObjectURL(remoteStream);
+        video.play();
+      });
+    }, function(err) {
+      console.log('Failed to get local stream' ,err);
+    });
   });
 }
+
+
 
 function connectTo(id){
   connection = peer.connect(id);
@@ -60,4 +77,20 @@ function connectTo(id){
 
 function sendMessage(message){
   connection.send(message);
+}
+
+function connectVideo(id){
+  navigator.getUserMedia({video: true, audio: true}, function(stream) {
+    var call = peer.call(id, stream);
+
+    call.on('stream', function(remoteStream) {
+      console.log('Appel sortant' + remoteStream);
+
+      var video = document.getElementById('webrtcvideo');
+      video.src = (URL || webkitURL || mozURL).createObjectURL(remoteStream);
+      video.play();
+    });
+  }, function(err) {
+    console.log('Failed to get local stream' ,err);
+  });
 }
