a974595836a315c35f3c38e51f5827dfbcebf983
arnaud roche <arnaud@arnauds-MacBook-Pro.local>
2017-05-24 10:50:59 +0200

    fix init and register / login buttons on forgot password

--- js/init.js
+++ js/init.js
@@ -1,19 +1,5 @@
-let fs = require('fs');
-
-var SERVER_URL;
-var UPDATE_MESSAGE_TIME;
-var PSEUDO_TIME;
-var UPDATE_INVITE;
-var UPDATE_CONTACT;
-
-//Lecture du fichier de configuration
-fs.readFile('properties/configuration.json', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
   SERVER_URL = "http://localhost:8080/server";
   UPDATE_MESSAGE_TIME = 2000;
   PSEUDO_TIME = 50;
   UPDATE_INVITE = 5000;
   UPDATE_CONTACT = 5000;
-});
